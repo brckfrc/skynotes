c1168c16937cfbdbe7fba2580de578be08802ee5
BoraKocabiyik <borakocabiyik@hotmail.com>
2025-05-21 19:52:58 +0300

    feat: Add more debug logs for BASE_URL in constants.js

--- frontend/skynotes-frontend/src/utils/constants.js
+++ frontend/skynotes-frontend/src/utils/constants.js
@@ -1,1 +1,10 @@
+// src/utils/constants.js
+console.log("Constants.js loaded.");
+console.log(
+  "Value of import.meta.env.VITE_API_URL:",
+  import.meta.env.VITE_API_URL
+);
+
 export const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:8000";
+
+console.log("Value of BASE_URL after assignment:", BASE_URL);
